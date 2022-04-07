6d9210611baf32fa1aaadc7fcbcf57cb9608cce3
Khaled <khalegjr@gmail.com>
2022-04-07 16:07:12 -0300

    feat: Cria e implementa form de cadastro de momentos

--- src/app/components/pages/new-moments/new-moments.component.ts
+++ src/app/components/pages/new-moments/new-moments.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-moments',
   templateUrl: './new-moments.component.html',
-  styleUrls: ['./new-moments.component.css']
+  styleUrls: ['./new-moments.component.css'],
 })
 export class NewMomentsComponent implements OnInit {
+  btnText: string = 'Compartilhar!';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
